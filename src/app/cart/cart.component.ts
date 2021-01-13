09cd01d87bdf43ec3b99138d7e2fa46070cd8203
Ezra97 <estollon@gmail.com>
2021-01-14 00:26:12 +0200

    added

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,62 @@
+import { TagPlaceholder } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+//import { threadId } from 'worker_threads';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CartComponent implements OnInit {
+  cartProducts:any;
+  bill:any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initiateData();
+  }
+initiateData(){
+  let data = localStorage.getItem('cart');
+  if(data!==null){
+    this.cartProducts=JSON.parse(data);
+    this.bill=0;
+    for(let i in this.cartProducts){
+      this.cartProducts[i]["qt"]=1;
+      this.bill = this.bill + this.cartProducts[i].price * this.cartProducts[i].qt;
+    }
+  }else{
+    this.cartProducts=[];
+  }
+}
+updateTotal(){
+this.bill=0;
+for(let i in this.cartProducts){
+  if(this.cartProducts[i].qt<1||this.cartProducts[i].qt>100){
+    alert("Quantity not in range");
+    return;
+  }
+    else{
+      this.bill = this.bill + this.cartProducts[i].price * this.cartProducts[i].qt;
+}
+}
+}
+removeItem(id: any){
+  this.cartProducts.splice(id,1);
+  if(this.cartProducts.length){
+    localStorage.setItem('cart',JSON.stringify(this.cartProducts));
+  }else{
+    localStorage.setItem('cart',null);
+  }
+}
+payBill(){
+  if(this.cartProducts.length){
+    localStorage.removeItem('cart');
+    this.initiateData();
+    alert("Your bill is: " + this.bill);
+  }
+  else{
+    alert("No items in cart");
+  }
+}
+}
